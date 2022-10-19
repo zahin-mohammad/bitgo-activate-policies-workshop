95b39f8d1b64643a43a15ff56aa28e419b064d4c
Zahin Mohammad <zahinmohammad@bitgo.com>
2022-10-19 09:35:28 -0400

    fix: use short lived token for create wallet

--- src/create-wallet.ts
+++ src/create-wallet.ts
@@ -1,74 +1,91 @@
 import { BitGo } from "bitgo";
 import * as inquirer from "inquirer";
 
+const bitgo = new BitGo({ env: "test" });
+
 type CreateWalletParams = {
-	bitgoAccessToken: string;
+	loginPassword: string;
 	coin: string;
 	walletPassPhrase: string;
-  enterpriseId: string;
-  user2Email: string;
+	enterpriseId: string;
+	user2Email?: string;
 };
+
 async function createWallet({
-	bitgoAccessToken,
+	loginPassword,
 	coin,
 	walletPassPhrase,
-  enterpriseId,
-  user2Email,
+	enterpriseId,
+	user2Email,
 }: CreateWalletParams) {
-  const bitgo = new BitGo({accessToken: bitgoAccessToken, env: 'test'});
-  await bitgo.unlock({otp: '000000'});
-  const bitgoCoin = bitgo.coin(coin);
-  const multisigType = bitgoCoin.supportsTss ? 'tss' : bitgoCoin.supportsBlsDkg ? 'blsdkg' : 'onchain';
+	walletPassPhrase = walletPassPhrase ? walletPassPhrase : loginPassword;
+	const bitgoCoin = bitgo.coin(coin);
+	const multisigType = bitgoCoin.supportsTss
+		? "tss"
+		: bitgoCoin.supportsBlsDkg
+		? "blsdkg"
+		: "onchain";
 
-  console.log("\n\ncreating wallet, this might take a few seconds....");
-  const res = await bitgoCoin.wallets().generateWallet({
-    enterprise: enterpriseId,
-    label: 'Hello Activate ' + new Date().toString(),
-    passphrase: walletPassPhrase,
-    multisigType,
-  });
-  console.log(res);
-  console.log('walletId', res.wallet.id());
+	console.log("\n\ncreating wallet, this might take a few seconds....");
+	await bitgo.unlock({ otp: "000000" });
+	const res = await bitgoCoin.wallets().generateWallet({
+		enterprise: enterpriseId,
+		label: "Hello Activate " + new Date().toString(),
+		passphrase: walletPassPhrase,
+		multisigType,
+	});
+	console.log(res);
+	console.log("walletId", res.wallet.id());
 
-  console.log(`\n\nsharing wallet with ${user2Email}`)
-  await res.wallet.shareWallet({
-    email: user2Email,
-    walletPassphrase: walletPassPhrase,
-    permissions: 'view,spend,admin',
-  });
+	console.log(`\n\nsharing wallet with ${user2Email}`);
+	await res.wallet.shareWallet({
+		email: user2Email,
+		walletPassphrase: walletPassPhrase,
+		permissions: "view,spend,admin",
+	});
 }
 
 const prompts = [
 	{
+		type: "input",
+		name: "loginEmail",
+		message: "BitGo testnet login email:",
+		validate: async function (email) {
+			if (!email) {
+				return "Email is required!";
+			}
+			return true;
+		},
+	},
+	{
 		type: "password",
-		name: "bitgoAccessToken",
-		message: "BitGo testnet access token:",
-		validate: async function (accessToken) {
-			const bitgo = new BitGo({
-				accessToken,
-				env: "test",
-			});
+		name: "loginPassword",
+		message: "BitGo testnet login password:",
+		validate: async function (loginPassword, answers) {
 			try {
-        const user = await bitgo.me();
-        console.log(`welcome back ${user.name.full}`);
+				await bitgo.authenticate({
+					username: answers.loginEmail,
+					password: loginPassword,
+					otp: "000000",
+				});
+				const user = await bitgo.me();
+				console.log(`welcome back ${user.name.full}`);
 				return true;
 			} catch (e) {
 				return `Failed to authenticate, ${e}`;
 			}
 		},
 	},
-  {
+	{
 		type: "input",
 		name: "enterpriseId",
 		message: "BitGo testnet enterpriseId:",
 		validate: async function (enterpriseId, answers) {
-			const bitgo = new BitGo({
-				accessToken: answers.bitgoAccessToken,
-				env: "test",
-			});
 			try {
-        const enterprise = await bitgo.get(bitgo.url(`/enterprise/${enterpriseId}`, 2));
-        console.log(` enterprise ${enterprise.body.name}`);
+				const enterprise = await bitgo.get(
+					bitgo.url(`/enterprise/${enterpriseId}`, 2)
+				);
+				console.log(` enterprise ${enterprise.body.name}`);
 				return true;
 			} catch (e) {
 				return `Failed to fetch enterprise, ${e}`;
@@ -100,34 +117,32 @@ const prompts = [
 	{
 		type: "password",
 		name: "walletPassPhrase",
-		message: "Wallet passphrase:",
-		validate: function (passphrase) {
-      if (!passphrase || passphrase === "") {
-        return 'Passphrase can\'t be empty!';
-      }
-      return true;
-		},
+		message:
+			"Use a separate Wallet passphrase? (optional, login password will be used by default):",
 	},
-  {
+	{
 		type: "password",
 		name: "walletPassPhraseValidate",
 		message: "Re-enter passphrase:",
+		when: function (answers) {
+			return answers.walletPassPhrase;
+		},
 		validate: function (passphraseValidate, answers) {
-      if (passphraseValidate !== answers.walletPassPhrase) {
-        return 'Passphrase must match!';
-      }
-      return true;
+			if (passphraseValidate !== answers.walletPassPhrase) {
+				return "Passphrase must match!";
+			}
+			return true;
 		},
 	},
-  {
+	{
 		type: "input",
 		name: "user2Email",
-		message: "Invite user? (email):",
+		message: "Invite user (email):",
 		validate: function (user2Email) {
-      if (!user2Email || user2Email === "") {
-        return 'Must invite another user for this demo :)';
-      }
-      return true;
+			if (!user2Email || user2Email === "") {
+				return "Must invite another user for this demo :)";
+			}
+			return true;
 		},
 	},
 ];
@@ -135,6 +150,6 @@ const prompts = [
 if (require.main === module) {
 	inquirer.prompt(prompts).then(async (answers) => {
 		// console.log(JSON.stringify(answers, null, "  "));
-    await createWallet(answers);
+		await createWallet(answers);
 	});
 }
